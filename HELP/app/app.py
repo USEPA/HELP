878e4e72516bb044cf9382806fdc6418b727f768
Daniel Lee Young, Ph.D <young.daniel@epa.gov>
2019-08-01 14:53:19 -0400

    Added admin.py files and app.py.

---
+++ HELP/app/app.py
@@ -0,0 +1,24 @@
+# apps.py (app)
+# !/usr/bin/env python3
+# coding=utf-8
+# young.daniel@epa.gov
+
+"""
+This file is created to include any application configuration for the app.
+
+Using this, you can configure some of the attributes of the application.
+
+Available functions:
+It is the recommended best practice to place your application configuration
+in the apps.py.
+"""
+
+from django.apps import AppConfig
+
+
+class AccountsConfig(AppConfig):
+    """App configuration."""
+
+    name = 'accounts'
+    verbose_name = "accounts"
+    default_app_config = 'accounts.apps.AccountsConfig'
