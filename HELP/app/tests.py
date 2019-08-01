8964618bc4e7a1606d43ad1640f1fbfe747d7633
Daniel Lee Young, Ph.D <young.daniel@epa.gov>
2019-08-01 07:52:36 -0400

    Added 'dashboard' and 'support' templates to tool. Cleaned up code to meet PEP8/257.

--- HELP/app/tests.py
+++ HELP/app/tests.py
@@ -5,14 +5,16 @@
 # We skip this file because it wasn't written by/for EPA.
 
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+This file demonstrates writing tests using the unittest module.
+
+These will pass when you run "manage.py test".
 """
 
 import django
 from django.test import TestCase
 
 # TODO: Configure your database in settings.py and sync before running tests.
+
 
 class ViewTest(TestCase):
     """Tests for the application views."""
@@ -21,6 +23,7 @@ class ViewTest(TestCase):
         # Django 1.7 requires an explicit setup() when running tests in PTVS
         @classmethod
         def setUpClass(cls):
+            """Add docstring."""  # TODO add docstring.
             super(ViewTest, cls).setUpClass()
             django.setup()
 
@@ -38,3 +41,13 @@ class ViewTest(TestCase):
         """Tests the about page."""
         response = self.client.get('/about')
         self.assertContains(response, 'About', 3, 200)
+
+    def test_dashboard(self):
+        """Tests the dashboard page."""
+        response = self.client.get('/dashboard')
+        self.assertContains(response, 'Dashboard', 3, 200)
+
+    def test_support(self):
+        """Tests the support page."""
+        response = self.client.get('/support')
+        self.assertContains(response, 'Support', 3, 200)
