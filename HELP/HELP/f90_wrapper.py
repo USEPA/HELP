c077546872c8bbd894134ba2d1134946df413d53
Daniel Lee Young, Ph.D <young.daniel@epa.gov>
2019-08-01 15:09:27 -0400

    Added F2python==1.5.0 and f2py-test-package==0.1 to test fortran 90 help code in wrapper.

---
+++ HELP/HELP/f90_wrapper.py
@@ -0,0 +1,13 @@
+# f90_wrapper.py (help)
+# !/usr/bin/env python3
+# coding=utf-8
+# young.daniel@epa.gov
+
+"""
+Numpy gfortran wrapper.
+
+Python with NumPy (Fortran 90 wrapper).
+
+https://www.fortran90.org/src/rosetta.html
+https://www.numfys.net/howto/F2PY/
+"""
