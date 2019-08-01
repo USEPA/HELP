3ff7b7098bd4debebb25a87f234454075368531e
Daniel Lee Young, Ph.D <young.daniel@epa.gov>
2019-08-01 15:13:41 -0400

    Updated admin and app files still need to fix login... help f90 wrapper to go in dashboard.

--- HELP/app/admin.py
+++ HELP/app/admin.py
@@ -14,25 +14,3 @@ be modified by an admin user.
 Available functions:
 - None for this module -- TBD (would like added to manage in Django Admin)
 """
-
-from django.contrib import admin
-from app.models import UserProfile, UserGroup, Country, State, Sector, Role
-
-admin.site.register(UserGroup)
-admin.site.register(Country)
-admin.site.register(State)
-admin.site.register(Sector)
-admin.site.register(Role)
-
-
-class UserProfileAdmin(admin.ModelAdmin):
-    """Assigns 'USER ID' number, once user registers with HELP."""
-
-    list_display = ("user_id",)
-    search_fields = ("user__username",)
-    exclude = ('created_by', 'last_modified_by',)
-    list_filter = ("user_id",)
-    list_per_page = 25
-
-
-admin.site.register(UserProfile, UserProfileAdmin)
