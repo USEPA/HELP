a9d637a6b9318ab9e1f99904cb7eeabf9caff931
Daniel Lee Young, Ph.D <young.daniel@epa.gov>
2019-08-02 11:09:50 -0400

    Added login and um...

--- HELP/HELP/urls.py
+++ HELP/HELP/urls.py
@@ -9,7 +9,7 @@
 """Definition of urls for HELP."""
 
 from datetime import datetime
-from django.urls import path
+from django.urls import include, path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from HELP import forms, views
@@ -21,19 +21,20 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('support/', views.support, name='support'),
-    path('FortranC/', views.FortranC, name='Software'),
-    path('login/',
-         LoginView.as_view
-         (
-             template_name='accounts/login.html',
-             authentication_form=forms.BootstrapAuthenticationForm,
-             extra_context=
-             {
-                 'title': 'Log in',
-                 'year': datetime.now().year,
-             }
-         ),
-         name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('FortranC/', views.FortranC, name='FortranC'),
+    #path('login/',
+    #     LoginView.as_view
+    #     (
+    #         template_name='accounts/login.html',
+    #         authentication_form=forms.BootstrapAuthenticationForm,
+    #         extra_context=
+    #         {
+    #             'title': 'Log in',
+    #             'year': datetime.now().year,
+    #         }
+    #     ),
+    #     name='login'),
+    #path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
 ]
