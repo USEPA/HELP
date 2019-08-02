4e5a562898886507320e9d915a93873ade19e50f
Jacob S <jacob@gqc.com>
2019-08-02 12:38:29 -0400

    Added support page and manual download.

--- HELP/HELP/forms.py
+++ HELP/HELP/forms.py
@@ -7,18 +7,35 @@
 """Definition of forms."""
 
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
+from HELP.util_models import Ticket
 
 
-class BootstrapAuthenticationForm(AuthenticationForm):
-    """Authentication form which uses boostrap CSS."""
+class TicketForm(forms.Form):
+    """@class TicketForm. Form for submitting ticket issues."""
 
-    username = forms.CharField(max_length=254,
-                               widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'User name'}))
-    password = forms.CharField(label=_("Password"),
-                               widget=forms.PasswordInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'Password'}))
+    INQUIRY_CHOICES = (
+        ("Request to Add a New Feature", "Request to Add a New Feature"),
+        ("Bug Tracking", "Bug Tracking"))
+    issue = forms.CharField(label=_("Issue:"), required=True,
+                            widget=forms.TextInput(
+                                attrs={'class': 'form-control'}))
+    subject = forms.CharField(label=_("Subject:"), required=True,
+                              widget=forms.TextInput(
+                                  attrs={'class': 'form-control'}))
+    type = forms.ChoiceField(label=_("Inquiry Type:"), choices=INQUIRY_CHOICES,
+                             widget=forms.Select(
+                                 attrs={'class': 'form-control'}),
+                             required=False)
+    description = forms.CharField(label=_("Problem Description:"),
+                                  required=True, widget=forms.Textarea(
+                                      attrs={'class': 'form-control', 'rows': 5}))
+    email = forms.EmailField(label=_("Your Email:"), required=True,
+                             widget=forms.TextInput(
+                                 attrs={'class': 'form-control'}))
+
+    class Meta:
+        """Ticket fields for support."""
+
+        model = Ticket
+        fields = ('issue', 'subject', 'type', 'description', 'email')
---
+++ HELP/HELP/util_models.py
@@ -0,0 +1,22 @@
+
+# util_models.py (HELP)
+# !/usr/bin/env python3
+# coding=utf-8
+# young.daniel@epa.gov
+
+"""
+@file util_models.py.
+
+Utility models/classes for the HELP application.
+The models in this file do not belong in the database and are kept
+separate from the other Django Models.
+"""
+
+class Ticket:
+    """@class Ticket. Represents an issue ticket submitted by a user."""
+
+    issue = str()
+    subject = str()
+    type = str()
+    description = str()
+    email = str()
