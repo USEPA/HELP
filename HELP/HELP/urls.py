10c216582fd81edf1e58a9b8d8e4f1f298908f9d
Daniel Lee Young, Ph.D <young.daniel@epa.gov>
2019-08-02 08:34:41 -0400

    Changed f90_wrapper to FortranC with addition info on converting the original 1992 f90 code to C and running in a Django web app using wrappers.

--- HELP/HELP/urls.py
+++ HELP/HELP/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('support/', views.support, name='support'),
-    path('f90_wrapper/', views.f90_wrapper, name='Software'),
+    path('FortranC/', views.FortranC, name='Software'),
     path('login/',
          LoginView.as_view
          (
