ef965fee84f34f5187247c8be2df12d251525f6f
Daniel L. Young, Ph.D <young.daniel@epa.gov>
2019-08-09 12:17:02 -0400

    Added .py files for f77/90 wrappers.

---
+++ HELP/HELP/ascimisc_for.py
@@ -0,0 +1,9 @@
+# ascimisc_for.py (help)
+# !/usr/bin/env python3
+# coding=utf-8
+# young.daniel@epa.gov
+
+
+"""
+Used to print output to file in a grid format. References cells, rows, cols, etc.
+"""
---
+++ HELP/HELP/ascirad_for.py
@@ -0,0 +1,12 @@
+# ascirad_for.py (help)
+# !/usr/bin/env python3
+# coding=utf-8
+# young.daniel@epa.gov
+
+"""
+ROUTINE CONVERTS AN ASCII FILE TO A FORMATTED FILE CONTAINING DAILY VALUES.
+
+EACH LINE OF OUTPUT CONSISTS OF THE YEAR, TEN DATA VALUES, AND THE LINE NUMBER.
+PROGRAM SCANS
+Solar Radiation
+"""
