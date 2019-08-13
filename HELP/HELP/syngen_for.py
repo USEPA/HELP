4e213dc6fe491ec25f2488ebfd9bf69aa88eab13
dyoung11 <young.daniel@epa.gov>
2019-08-13 09:13:59 -0400

    Minor pep 8/257 update...

--- HELP/HELP/syngen_for.py
+++ HELP/HELP/syngen_for.py
@@ -4,13 +4,14 @@
 # young.daniel@epa.gov
 
 """
-Routine prepares climatological input files from the synthetic climatological data tape.
+Routine prepares climatological input files from the synthetic climatological
+data tape.
 
 Available functions:
 - TAPE1 contains the synthetic rain parameters, alpha and beta.
 - DATA4 contains the selected daily precipitation values.
 - DATA7 contains the selected average daily temperatures.
-- DATA11 contains the name of the selected city, the evaporative zone depth, the
-         vegetation type, maz lai, planting and harvesting dates.
+- DATA11 contains the name of the selected city, the evaporative zone depth
+         the vegetation type, maz lai, planting and harvesting dates.
 - DATA13 contains the selected solar radiation values.
 """
